71a0598e79afc3de57191be2431997439b214e10
AMchuAMchuAMchu <2466130840@qq.com>
2023-03-07 19:50:29 +0800

    django数据库连接

--- myapp/views.py
+++ myapp/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 from django.shortcuts import render
 
+from myapp import models
 
 user_list = [
     {'username':'雪之下雪乃','password':'5201314'},
@@ -11,8 +12,14 @@ user_list = [
 ]
 
 def index(request):
+    global user_list
     if request.method == 'POST':
-        if request.POST.get('username') is not '' and request.POST.get('password') is not '':
-            user = {'username':request.POST.get('username'),'password':request.POST.get('password')}
+        if request.POST.get('username') != '' and request.POST.get('password') != '':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = {'username':username,'password':password}
             user_list.append(user)
+            models.UserInfo.objects.create(username=username,password=password)
+            userInfoQuerySet = models.UserInfo.objects.all()
+            user_list = list(userInfoQuerySet)
     return render(request,'index.html',{'data':user_list})
